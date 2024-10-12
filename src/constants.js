08d5fd590c64887e01989ebb5773397d7b022f15
vinay kumar <vinaykumar@Vinays-MacBook-Air.local>
2024-10-12 16:48:10 +0530

    dev

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 3;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in backend. I have created projects in Java, Python, C/C++. 
+    Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/vinay-kumar07" target="_blank">Github</a>!`,
+    "cs-degree": `<b>EDUCATION</b> </br> I graduated from Computer Science Department of IIT Ropar in 2024 with a CGPA of 8.10/10.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube and Netflix. I also like to read books here.`,
+    tv: `<b>Netflix and Chill!!</b>`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+            Contact me at vinaykr487@gmail.com if you have any interesting job opportunities!`,
+    projects: `<b>EXPERIENCE</b> </br>
+                I have previously worked as a backend dev a at nurture.farm, kroop AI and as a research intern at IIT Mandi.`,
+    library: `<b>PUBLICATIONS</b> </br> I have worked in the domian of Human-in-the-loop and the work is published in ECAI 2024`,
+    exit: `If you want to exit the portfolio, just close the tab.`,
+};
